daa14d1851010d435119553031b3132321934593
harshmohan07 <code.harshmohan@gmail.com>
2022-07-23 19:01:18 +0530

    models deployment link changed

--- web-app/src/components/AppSidebarNav.js
+++ web-app/src/components/AppSidebarNav.js
@@ -62,7 +62,7 @@ export const AppSidebarNav = ({ items }) => {
       <CNavItem>
         <CNavLink
           target="_black"
-          href="https://vedanshi-shah-covigilance-main-dev-vedanshi-3aho1z.streamlitapp.com/"
+          href="https://vedanshi-shah-covigilance-main-models-cje3cw.streamlitapp.com/"
         >
           <CIcon
             style={{ marginRight : "20px" }}
